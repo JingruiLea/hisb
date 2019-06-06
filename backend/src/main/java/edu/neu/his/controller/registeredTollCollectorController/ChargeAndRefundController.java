6dc2a3cf2101d951d6725d7be9015a3ee6273428
Administrator <admin@example.com>
2019-06-06 11:21:29 +0800

    '4.2/4.4'

--- backend/src/main/java/edu/neu/his/controller/registeredTollCollectorController/ChargeAndRefundController.java
+++ backend/src/main/java/edu/neu/his/controller/registeredTollCollectorController/ChargeAndRefundController.java
@@ -71,7 +71,7 @@ public class ChargeAndRefundController {
 
         if(!IDsHaveCharged.isEmpty() || !IDsNotHave.isEmpty()){
             data.put("ids_not_have",IDsNotHave);
-            data.put("ids_have_charged",IDsHaveCharged);
+            data.put("ids_have_charged_or_refunded",IDsHaveCharged);
             return Response.Error(data);
         }else {
             //生成票据
@@ -95,6 +95,71 @@ public class ChargeAndRefundController {
                 SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
                 String create_time = df.format(new Date());
                 OperateLog operateLog = new OperateLog((int)req.get("_uid"),medical_record_id,operateType,bill_record_id,outpatientChargesRecord.getCost(),create_time);
+                operateLogService.insertOperateLog(operateLog);
+            });
+
+            return Response.Ok();
+        }
+    }
+
+    @PostMapping("/return")
+    @ResponseBody
+    public Map refund(@RequestBody Map req){
+        OperateStatus.initOperateMap();
+
+        Map data = new HashMap();
+        List<Integer> IDsNotHave = new ArrayList<>();
+        List<Integer> IDsNotCharged = new ArrayList<>();
+        List<OutpatientChargesRecord> recordsToRefund = new ArrayList<>();
+        float cost = 0;
+        String type = BillRecordStatus.Refund;
+        int medical_record_id = (int)req.get("medical_record_id");
+
+        String str = (String)req.get("charges_id_list");
+        int beginIndex = str.indexOf('[') == 0 ? 1 : 0;
+        int endIndex = str.lastIndexOf(']') + 1 == str.length() ? str.lastIndexOf(']') : str.length();
+        str = str.substring(beginIndex, endIndex);
+        String[] id_list = str.split(",");
+
+        for(int i=0; i<id_list.length; i++){
+            int id = Integer.parseInt(id_list[i]);
+            OutpatientChargesRecord outpatientChargesRecord = chargeAndRefundService.findByMedicalRecordIdAndId(medical_record_id,id);
+            if(outpatientChargesRecord==null){
+                IDsNotHave.add(id);
+            }else if(!outpatientChargesRecord.getStatus().equals(OutpatientChargesRecordStatus.Charged)){
+                IDsNotCharged.add(id);
+            }else {
+                recordsToRefund.add(outpatientChargesRecord);
+                cost -= outpatientChargesRecord.getCost();
+            }
+        }
+
+        if(!IDsNotCharged.isEmpty() || !IDsNotHave.isEmpty()){
+            data.put("ids_not_have",IDsNotHave);
+            data.put("ids_not_charged_or_have_refunded",IDsNotCharged);
+            return Response.Error(data);
+        }else {
+            //生成票据
+            BillRecord billRecord = reqToBillRecord(req,type,cost);
+            int bill_record_id = billRecordService.insertBillRecord(billRecord);
+
+            //修改收费记录
+            recordsToRefund.forEach(outpatientChargesRecord -> {
+                outpatientChargesRecord.setStatus(OutpatientChargesRecordStatus.Refunded);
+                outpatientChargesRecord.setBill_record_id(bill_record_id);
+                chargeAndRefundService.update(outpatientChargesRecord);
+
+                //对每条收费记录生成对应的操作记录
+                String operateType;
+                if(outpatientChargesRecord.getType().equals("药品"))
+                    operateType = OperateStatus.PrescribeMedicine;
+                else {
+                    int expenseClassificationId = outpatientChargesRecord.getExpense_classification_id();
+                    operateType = OperateStatus.operateMap.get(expenseClassificationId);
+                }
+                SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                String create_time = df.format(new Date());
+                OperateLog operateLog = new OperateLog((int)req.get("_uid"),medical_record_id,operateType,bill_record_id,0-outpatientChargesRecord.getCost(),create_time);
                 operateLogService.insertOperateLog(operateLog);
             });
 
