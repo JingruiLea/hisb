a7db460c416ba2a2a04dda3a008c4347425d0d81
Administrator <admin@example.com>
2019-06-11 18:40:21 +0800

    '5.4'

--- backend/src/main/java/edu/neu/his/mapper/BillRecordMapper.java
+++ backend/src/main/java/edu/neu/his/mapper/BillRecordMapper.java
@@ -10,15 +10,15 @@ import java.util.List;
 @Component(value = "BillRecordMapper")
 public interface BillRecordMapper {
     @Insert("INSERT INTO bill_record(medical_record_id, type , print_status, " +
-            "cost, should_pay, truely_pay, retail_fee, user_id, creat_time) " +
+            "cost, should_pay, truely_pay, retail_fee, user_id, create_time) " +
         "VALUES(#{medical_record_id}, #{type}, #{print_status}, " +
-            "#{cost}, #{should_pay}, #{truely_pay}, #{retail_fee}, #{user_id}, #{creat_time})")
+            "#{cost}, #{should_pay}, #{truely_pay}, #{retail_fee}, #{user_id}, #{create_time})")
     @Options(useGeneratedKeys = true, keyProperty = "id")
     void insert(BillRecord billRecord);
 
     @Select("SELECT * from bill_record where id = #{id}")
     BillRecord find(@Param("id") int id);
 
-    @Select("SELECT * from bill_record where user_id = #{user_id} and creat_time > #{start_time} and creat_time < #{end_time}")
+    @Select("SELECT * from bill_record where user_id = #{user_id} and create_time > #{start_time} and create_time < #{end_time}")
     List<BillRecord> findByUserIdAndTime(@Param("user_id") int user_id, @Param("start_time") String start_time, @Param("end_time") String end_time);
 }
