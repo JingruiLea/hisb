8275cc99cc9c8dfce10f85b98b1a3cd03e3479d0
puddingTree <460986787@qq.com>
2019-06-14 16:40:22 +0800

    n

---
+++ backend/src/main/java/edu/neu/his/bean/dailyCheck/DailyCheckController.java
@@ -0,0 +1,10 @@
+package edu.neu.his.bean.dailyCheck;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/outpatientDailyReportCheck")
+public class DailyCheckController {
+
+}
