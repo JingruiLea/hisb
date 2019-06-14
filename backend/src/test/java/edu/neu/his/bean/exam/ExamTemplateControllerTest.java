06fc197f4e908ae18d9dca58afdfe761e6ac4ff3
klmyty <1152113457@qq.com>
2019-06-14 16:26:21 +0800

    Merge branch 'ldy' of https://github.com/XUranus/his into ldy
    # Conflicts:
    #	backend/src/main/java/edu/neu/his/auto/CommonDiagnosisMapper.java
    #	backend/src/main/java/edu/neu/his/bean/department/DepartmentManageController.java
    #	backend/src/main/java/edu/neu/his/bean/dignosis/DiagnosisTemplate.java
    #	backend/src/main/java/edu/neu/his/bean/dignosis/DiagnosisTemplateService.java
    #	backend/src/main/java/edu/neu/his/bean/drug/DrugService.java
    #	backend/src/main/java/edu/neu/his/bean/medicalRecord/MedicalRecordTemplateController.java
    #	backend/src/main/java/edu/neu/his/bean/outpatientCharges/ChargeAndRefundController.java
    #	backend/src/main/java/edu/neu/his/bean/prescription/PrescriptionService.java
    #	backend/src/main/java/edu/neu/his/bean/prescription/PrescriptionTemplateService.java
    #	backend/src/main/java/edu/neu/his/bean/user/UserManagementController.java
    #	backend/src/test/java/edu/neu/his/controller/outpatientMedicalRecordController/ExamControllerTest.java
    #	backend/src/test/resources/test_data.json

--- backend/src/test/java/edu/neu/his/bean/exam/ExamTemplateControllerTest.java
+++ backend/src/test/java/edu/neu/his/bean/exam/ExamTemplateControllerTest.java
@@ -1,4 +1,4 @@
-package edu.neu.his.controller.outpatientMedicalRecordController;
+package edu.neu.his.bean.exam;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,22 +20,72 @@ import static org.junit.Assert.*;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-public class ExamControllerTest {
-    private Logger logger = LoggerFactory.getLogger(ExamControllerTest.class);
+public class ExamTemplateControllerTest {
+    private Logger logger = LoggerFactory.getLogger(ExamTemplateControllerTest.class);
 
     @Autowired
     protected MockMvc mockMvc;
 
     @Test
+    public void list() throws  Exception{
+        logger.info("MockMvcResultMatchers.status().isOk()", MockMvcResultMatchers.status().isOk());
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.
+                post("/examTemplate/list")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(" {\n" +
+                        "      \"type\" : 1,\n" +
+                        "      \"_uid\":10004\n" +
+                        "    }")
+
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+    }
+
+    @Test
     public void create() throws  Exception{
         logger.info("MockMvcResultMatchers.status().isOk()", MockMvcResultMatchers.status().isOk());
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.
-                post("/exam/getOrCreate")
+                post("/examTemplate/create")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(" {\n" +
-                        "      \"type\":0,\n" +
-                        "      \"medical_record_id\" : 1,\n" +
-                        "      \"non_drug_id_list\":[5],\n" +
+                        "      \"template_name\": \"常规检查3\",\n" +
+                        "      \"type\": 2,\n" +
+                        "      \"_uid\": 10004,\n" +
+                        "      \"non_drug_id_list\": [11,12]\n" +
+                        "    }")
+
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+    }
+
+    @Test
+    public void detail() throws  Exception{
+        logger.info("MockMvcResultMatchers.status().isOk()", MockMvcResultMatchers.status().isOk());
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.
+                post("/examTemplate/detail")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(" {\n" +
+                        "      \"exam_template_id\": 2\n" +
+                        "    }")
+
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+    }
+
+    @Test
+    public void delete() throws  Exception{
+        logger.info("MockMvcResultMatchers.status().isOk()", MockMvcResultMatchers.status().isOk());
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.
+                post("/examTemplate/submit")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(" {\n" +
+                        "      \"exam_id\" : 1,\n" +
                         "      \"_uid\":10005\n" +
                         "    }")
 
@@ -49,24 +99,7 @@ public class ExamControllerTest {
     public void addOne() throws  Exception{
         logger.info("MockMvcResultMatchers.status().isOk()", MockMvcResultMatchers.status().isOk());
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.
-                post("/exam/add")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(" {\n" +
-                        "      \"exam_id\" : 3,\n" +
-                        "      \"non_drug_id\" : [9]\n" +
-                        "    }")
-
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andDo(MockMvcResultHandlers.print())
-                .andReturn();
-    }
-
-    @Test
-    public void submit() throws  Exception{
-        logger.info("MockMvcResultMatchers.status().isOk()", MockMvcResultMatchers.status().isOk());
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.
-                post("/exam/submit")
+                post("/examTemplate/submit")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(" {\n" +
                         "      \"exam_id\" : 1,\n" +
@@ -83,11 +116,11 @@ public class ExamControllerTest {
     public void delOne() throws  Exception{
         logger.info("MockMvcResultMatchers.status().isOk()", MockMvcResultMatchers.status().isOk());
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.
-                post("/exam/delete")
+                post("/examTemplate/submit")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(" {\n" +
                         "      \"exam_id\" : 1,\n" +
-                        "      \"non_drug_id\" : [2]\n" +
+                        "      \"_uid\":10005\n" +
                         "    }")
 
                 .accept(MediaType.APPLICATION_JSON))
