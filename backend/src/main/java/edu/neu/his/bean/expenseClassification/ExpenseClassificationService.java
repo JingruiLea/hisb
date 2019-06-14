d5694d776a06b04188251fc5a7ae474853b0f307
puddingTree <460986787@qq.com>
2019-06-14 16:26:42 +0800

    n

--- backend/src/main/java/edu/neu/his/bean/expenseClassification/ExpenseClassificationService.java
+++ backend/src/main/java/edu/neu/his/bean/expenseClassification/ExpenseClassificationService.java
@@ -3,6 +3,7 @@ package edu.neu.his.bean.expenseClassification;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
 
 @Service
 public class ExpenseClassificationService {
@@ -18,4 +19,40 @@ public class ExpenseClassificationService {
     public String findClassificationById(int id){
         return expenseClassificationMapper.findClassificationById(id);
     }
+
+    @Transactional
+    public void updateExpenseClassification(ExpenseClassification expenseClass) {
+        expenseClassificationMapper.update(expenseClass);
+    }
+
+    @Transactional
+    public void insertExpenseClass(ExpenseClassification expenseClass) {
+        expenseClassificationMapper.insert(expenseClass);
+    }
+
+    @Transactional
+    public List<ExpenseClassification> findAll() {
+        return expenseClassificationMapper.findAll();
+    }
+
+    @Transactional
+    public void deleteExpenseClassification(int id) {
+        expenseClassificationMapper.delete(id);
+    }
+
+    @Transactional
+    public boolean exist(ExpenseClassification expenseClassification) {
+        return expenseClassificationMapper.checkIdExistNums(expenseClassification)==1;
+    }
+
+    @Transactional
+    public boolean canDelete(int id) {
+        return expenseClassificationMapper.checkId(id)==1;
+    }
+
+    @Transactional
+    public String selectById(int id){
+        return expenseClassificationMapper.findClassificationById(id);
+    }
+
 }
