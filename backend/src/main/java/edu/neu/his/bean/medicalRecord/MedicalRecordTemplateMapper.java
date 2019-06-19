1d16ff64eb7a14f0f7e48f05bd3596c9242291cc
Administrator <admin@example.com>
2019-06-19 11:46:18 +0800

    status

--- backend/src/main/java/edu/neu/his/bean/medicalRecord/MedicalRecordTemplateMapper.java
+++ backend/src/main/java/edu/neu/his/bean/medicalRecord/MedicalRecordTemplateMapper.java
@@ -1,8 +1,6 @@
 package edu.neu.his.bean.medicalRecord;
 
-import edu.neu.his.bean.medicalRecord.MedicalRecordTemplate;
 import java.util.List;
-
 import org.apache.ibatis.annotations.*;
 import org.apache.ibatis.type.JdbcType;
 import org.springframework.stereotype.Component;
@@ -115,13 +113,13 @@ public interface MedicalRecordTemplateMapper {
     @Insert({
             "insert into medical_record_template (`title`, `type`, ",
             "user_id, department_id, ",
-            "create_time, `status`, ",
+            "create_time, ",
             "chief_complaint, current_medical_history, ",
             "current_treatment_situation, past_history, ",
             "allergy_history, physical_examination)",
             "values (#{title,jdbcType=VARCHAR}, #{type,jdbcType=INTEGER}, ",
             "#{user_id,jdbcType=INTEGER}, #{department_id,jdbcType=INTEGER}, ",
-            "#{create_time,jdbcType=VARCHAR}, #{status,jdbcType=VARCHAR}, ",
+            "#{create_time,jdbcType=VARCHAR},",
             "#{chief_complaint,jdbcType=LONGVARCHAR}, #{current_medical_history,jdbcType=LONGVARCHAR}, ",
             "#{current_treatment_situation,jdbcType=LONGVARCHAR}, #{past_history,jdbcType=LONGVARCHAR}, ",
             "#{allergy_history,jdbcType=LONGVARCHAR}, #{physical_examination,jdbcType=LONGVARCHAR})"
@@ -131,7 +129,7 @@ public interface MedicalRecordTemplateMapper {
 
     @Select({
             "select",
-            "id, `title`, `type`, user_id, department_id, create_time, `status`, chief_complaint, ",
+            "id, `title`, `type`, user_id, department_id, create_time, chief_complaint, ",
             "current_medical_history, current_treatment_situation, past_history, allergy_history, ",
             "physical_examination",
             "from medical_record_template",
@@ -144,7 +142,6 @@ public interface MedicalRecordTemplateMapper {
             @Result(column="user_id", property="user_id", jdbcType=JdbcType.INTEGER),
             @Result(column="department_id", property="department_id", jdbcType=JdbcType.INTEGER),
             @Result(column="create_time", property="create_time", jdbcType=JdbcType.VARCHAR),
-            @Result(column="status", property="status", jdbcType=JdbcType.VARCHAR),
             @Result(column="chief_complaint", property="chief_complaint", jdbcType=JdbcType.LONGVARCHAR),
             @Result(column="current_medical_history", property="current_medical_history", jdbcType=JdbcType.LONGVARCHAR),
             @Result(column="current_treatment_situation", property="current_treatment_situation", jdbcType=JdbcType.LONGVARCHAR),
@@ -156,7 +153,7 @@ public interface MedicalRecordTemplateMapper {
 
     @Select({
             "select",
-            "id, `title`, `type`, user_id, department_id, create_time, `status`, chief_complaint, ",
+            "id, `title`, `type`, user_id, department_id, create_time, chief_complaint, ",
             "current_medical_history, current_treatment_situation, past_history, allergy_history, ",
             "physical_examination",
             "from medical_record_template"
@@ -168,7 +165,6 @@ public interface MedicalRecordTemplateMapper {
             @Result(column="user_id", property="user_id", jdbcType=JdbcType.INTEGER),
             @Result(column="department_id", property="department_id", jdbcType=JdbcType.INTEGER),
             @Result(column="create_time", property="create_time", jdbcType=JdbcType.VARCHAR),
-            @Result(column="status", property="status", jdbcType=JdbcType.VARCHAR),
             @Result(column="chief_complaint", property="chief_complaint", jdbcType=JdbcType.LONGVARCHAR),
             @Result(column="current_medical_history", property="current_medical_history", jdbcType=JdbcType.LONGVARCHAR),
             @Result(column="current_treatment_situation", property="current_treatment_situation", jdbcType=JdbcType.LONGVARCHAR),
@@ -185,7 +181,6 @@ public interface MedicalRecordTemplateMapper {
             "user_id = #{user_id,jdbcType=INTEGER},",
             "department_id = #{department_id,jdbcType=INTEGER},",
             "create_time = #{create_time,jdbcType=VARCHAR},",
-            "`status` = #{status,jdbcType=VARCHAR},",
             "chief_complaint = #{chief_complaint,jdbcType=LONGVARCHAR},",
             "current_medical_history = #{current_medical_history,jdbcType=LONGVARCHAR},",
             "current_treatment_situation = #{current_treatment_situation,jdbcType=LONGVARCHAR},",
