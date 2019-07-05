9ce15b1f89b1f699d4096f4f075d8d4eaac24c6c
jingruilea <1079805974@qq.com>
2019-07-05 10:03:12 +0800

    hah

--- backend/src/main/java/edu/neu/his/bean/user/UserInfoController.java
+++ backend/src/main/java/edu/neu/his/bean/user/UserInfoController.java
@@ -23,7 +23,7 @@ public class UserInfoController {
     public Map getUserInfo(@RequestBody Map req) {
         //System.out.println(req);
         int uid = (int)req.get("_uid");
-        User user = userService.findByUid(uid);
+        User user = userService.findBeyUid(uid);
         if(user!=null) {
             user.setPassword(null);
             Map res = Utils.objectToMap(user);
